666bc51067f3aebed2e48050de6d043aa7ac08bf
divaerzenia <erzeniad@gmail.com>
2022-09-05 07:12:53 -0700

    first commit

---
+++ script.js
@@ -0,0 +1,19 @@
+var x =10;
+console.log('Hello World !')
+console.log('isi dari variabel x adalah ' + x);
+
+alert('Halo ayang')
+alert('kepada ayang kepada ayang')
+alert('ini awak ayang')
+alert('ayang awak minta maaf, ayang jangan marah lagi ya')
+alert('awk ndak bermakcud buat ayang marah, awk cayang ayang')
+
+var nama = prompt('ayang mau maafin awk kan ?')
+
+var tes = confirm("ayang yakin ?");
+if( tes === true ) {
+	alert ('wahhhh timakaci ayanggggg, awk sayangggg ayangggg. lopyu ayang')
+} else {
+	alert('yacudah ndak papa ayang, awk bakal tetep nunggu ayang campe ayang maafin awk')
+}
+
